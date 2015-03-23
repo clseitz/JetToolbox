da43cbf171f04c009de7ddccbaa51c9be40cd431
Alejandro <gomez@rutgers.physics.edu>
2015-03-23 18:24:59 -0500

    adding general input

--- test/jettoolbox_cfg.py
+++ test/jettoolbox_cfg.py
@@ -15,7 +15,7 @@ process.MessageLogger.suppressWarning = cms.untracked.vstring('ecalLaserCorrFilt
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(True)
 
-from RecoJets.JetProducers.jetToolbox_cff import *
+from JetToolbox.jetToolbox_cff import *
 jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', addSubjets= True ) #, addGroomers = False )
 jetToolbox( process, 'ca8', 'ca8JetSubs', 'out', addSubjets=True, addNsub=True, addPruning=True, nfilt=5 , zCut=0.2, addTrimming=True, addCMSTopTagger=True, addHEPTopTagger=True, addMassDrop=True )
 jetToolbox( process, 'ca15', 'ca15JetSubs', 'out' )
@@ -25,10 +25,12 @@ jetToolbox( process, 'ca15', 'ca15JetSubs', 'out' )
 
 
 process.endpath = cms.EndPath(process.out)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+#process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('file:example.root') ) 
+#from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValProdTTbarAODSIM
+#process.source.fileNames = filesRelValProdTTbarAODSIM
 
 process.source = cms.Source("PoolSource",
-#		fileNames = cms.untracked.vstring('/store/user/jstupak/ZH_HToBB_ZToLL_M-125_13TeV_powheg-herwigpp/Spring14dr-PU_S14_POSTLS170_V6AN1-v1/140622_185946/0000/miniAOD-prod_PAT_1.root')
-#		fileNames = cms.untracked.vstring('/store/user/algomez/RPVSt100tojj_13TeV_pythia8_GENSIM/RPVSt100tojj_13TeV_pythia8_AODSIM_v706_PU40bx50/fffa6e37f9457f57c180027c6057d31f/RPVSt100tojj_13TeV_pythia8_AODSIM_PU40bx50_516_1_zGg.root')
-		fileNames = cms.untracked.vstring('/store/user/algomez/RPVSt100tojj_13TeV_pythia8_GENSIM/RPVSt100tojj_13TeV_pythia8_MiniAOD_v706_PU40bx25/b71e879835d2f0083a0e044b05216236/RPVSt100tojj_13TeV_pythia8_MiniAOD_PU40bx25_525_1_JVY.root')
+		fileNames = cms.untracked.vstring('/store/relval/CMSSW_7_2_2_patch1/RelValTTbar_13/MINIAODSIM/PU50ns_MCRUN2_72_V0-v1/00000/0683685E-8D73-E411-9A7A-0025905A60D0.root')
 		)
 
