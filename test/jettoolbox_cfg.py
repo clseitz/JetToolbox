db19c9fdb1e933a4da6782ad9c266b2d5356eea9
Claudia <clseitz@physics.rutgers.edu>
2015-03-31 17:02:15 +0200

    adding ak4 jets with trimming

--- test/jettoolbox_cfg.py
+++ test/jettoolbox_cfg.py
@@ -15,22 +15,20 @@ process.MessageLogger.suppressWarning = cms.untracked.vstring('ecalLaserCorrFilt
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(True)
 
-from cmsjet.JetToolbox.jetToolbox_cff import *
-jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', addSubjets= True ) #, addGroomers = False )
-jetToolbox( process, 'ca8', 'ca8JetSubs', 'out', addSubjets=True, addNsub=True, addPruning=True, nfilt=5 , zCut=0.2, addTrimming=True, addCMSTopTagger=True, addHEPTopTagger=True, addMassDrop=True )
-jetToolbox( process, 'ca15', 'ca15JetSubs', 'out' )
-#jetToolbox( process, 'ak8', 'ak8JetSubs', 'out', addSubjets= True, miniAOD=False ) #, addGroomers = False )
-#jetToolbox( process, 'ca8', 'ca8JetSubs', 'out', miniAOD=False, addSubjets=True, addNsub=True, addPruning=True, nfilt=5 , zCut=0.2 , addTrimming=True, addCMSTopTagger=True, addHEPTopTagger=True, addMassDrop=True )
-#jetToolbox( process, 'ca15', 'ca15JetSubs', 'out', miniAOD=False )
+from JMEAnalysis.JetToolbox.jetToolbox_cff import *
+jetToolbox( process, 'ak4', 'ak4JetSubs', 'out', addNsub=True, addPruning=True, nfilt=5 , zCut=0.2 ,addTrimming=True)
 
+
+#process.out = cms.OutputModule("PoolOutputModule",
+#                               fileName = cms.untracked.string('test.root'),
+#                               outputCommands = cms.untracked.vstring(['keep *'])
+#                               )
 
 process.endpath = cms.EndPath(process.out)
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
-#process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('file:example.root') ) 
-#from PhysicsTools.PatAlgos.patInputFiles_cff import filesRelValProdTTbarAODSIM
-#process.source.fileNames = filesRelValProdTTbarAODSIM
 
 process.source = cms.Source("PoolSource",
-		fileNames = cms.untracked.vstring('/store/mc/Spring14miniaod/QCD_Pt-300to470_Tune4C_13TeV_pythia8/MINIAODSIM/141029_PU40bx50_castor_PLS170_V6AN2-v1/00000/1A3D915A-CA7F-E411-AFE2-0025905A48D0.root')
-		)
+                            fileNames = cms.untracked.vstring('/store/mc/Spring14miniaod/QCD_Pt-300to470_Tune4C_13TeV_pythia8/MINIAODSIM/141029_PU40bx50_castor_PLS170_V6AN2-v1/00000/1A3D915A-CA7F-E411-AFE2-0025905A48D0.root')
+           
+                            )
 
